d5b9211f6135ebdcb9ef04d6c58f9b7301d3e9b1
metomatab-hoglund <metomatab@gmail.com>
2026-05-11 08:14:10 -1200

    Add files via upload

---
+++ hoglunds-kompeten-site/app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata = { title: 'Höglunds Kompeten', description: 'Battery Energy Storage & EMS Solutions' };
+export default function RootLayout({ children }) { return <html lang="en"><body>{children}</body></html>; }
